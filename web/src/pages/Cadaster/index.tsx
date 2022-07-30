68608c5211deed4fc6dfabaf4bcd80ce61fafd1d
David <david.henri.santana@gmail.com>
2022-07-30 18:23:17 -0300

    HOME lOGIN PAGE
    Eu queria namorar uma goth girl

--- web/src/pages/Cadaster/index.tsx
+++ web/src/pages/Cadaster/index.tsx
@@ -1,9 +1,6 @@
 export const Cadaster = () => {
-    async function handleCadaster() { /*TODO*/ }
-    return (
-      <>
-          <h1>ROUTE CADASTER</h1>
-      </>
-    );
-  };
-  
+  async function handleCadaster() {
+    /*TODO*/
+  }
+  return <h1>ROUTE CADASTER</h1>;
+};
--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,8 +1,33 @@
+import AlphaBunker from '../../assets/imgs/AlphaBunker.svg';
+
 export const Home = () => {
-  async function handleLogin() { /*TODO*/ }
   return (
-    <>
-        <h1>ROUTE HOME</h1>
-    </>
+    <div className="w-full h-full bg-body-light-200 flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center gap-4">
+        <img src={AlphaBunker}></img>
+        <h1 className="text-xl font-medium font-brand text-brand-base">
+          Alpha Bunker
+        </h1>
+      </div>
+      <h2 className="text-xl my-14 font-medium font-brand text-paragraph-dark">
+        Login
+      </h2>
+      <div className="flex flex-col items-center p-2.5 gap-5">
+        <input
+          className="w-[250px] h-[33px] rounded-md"
+          type="text"
+          placeholder="Digite seu CPF"
+        />
+        <input
+          className="w-[250px] h-[33px] rounded-md"
+          type="text"
+          placeholder="Digite sua senha"
+        />
+        <button className="bg-brand-base font-brand hover:bg-btn-primary-hover text-btn-text rounded-md w-[250px] h-[40px]">
+          Entrar
+        </button>
+        <p className="text-sm font-brand">Crie sua conta</p>
+      </div>
+    </div>
   );
 };
